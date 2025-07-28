436c1898a2008ea17e2023ded78827908147d364
tedrobinson <tfqbtd@aol.com>
2025-07-28 16:10:17 -0400

    End of section 12

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -2,11 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
 import { Member, Photo } from '../../../types/member';
-import { ImageUpload } from "../../../shared/image-upload/image-upload";
+import { ImageUpload } from '../../../shared/image-upload/image-upload';
 import { AccountService } from '../../../core/services/account-service';
 import { User } from '../../../types/user';
-import { StarButton } from "../../../shared/star-button/star-button";
-import { DeleteButton } from "../../../shared/delete-button/delete-button";
+import { StarButton } from '../../../shared/star-button/star-button';
+import { DeleteButton } from '../../../shared/delete-button/delete-button';
 
 @Component({
   selector: 'app-member-photos',
@@ -25,45 +25,54 @@ export class MemberPhotos implements OnInit {
     const memberId = this.route.parent?.snapshot.paramMap.get('id');
     if (memberId) {
       this.memberService.getMemberPhotos(memberId).subscribe({
-        next: photos => this.photos.set(photos)
-      })
+        next: (photos) => this.photos.set(photos),
+      });
     }
   }
 
   onUploadImage(file: File) {
     this.loading.set(true);
     this.memberService.uploadPhoto(file).subscribe({
-      next: photo => {
+      next: (photo) => {
         this.memberService.editMode.set(false);
         this.loading.set(false);
-        this.photos.update(photos => [...photos, photo]);
+        this.photos.update((photos) => [...photos, photo]);
+        if (!this.memberService.member()?.imageUrl) {
+          this.setMainLocal(photo);
+        }
       },
-      error: error => {
+      error: (error) => {
         console.log('Error uploading image: ', error);
         this.loading.set(false);
-      }
-    })
+      },
+    });
   }
 
   setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo).subscribe({
       next: () => {
-        const currentUser = this.accountService.currentUser();
-        if (currentUser) currentUser.imageUrl = photo.url;
-        this.accountService.setCurrentUser(currentUser as User);
-        this.memberService.member.update(member => ({
-          ...member,
-          imageUrl: photo.url
-        }) as Member)
-      }
-    })
+        this.setMainLocal(photo);
+      },
+    });
   }
 
   deletePhoto(photoId: number) {
     this.memberService.deletePhoto(photoId).subscribe({
       next: () => {
-        this.photos.update(photos => photos.filter(x => x.id !== photoId))
-      }
-    })
+        this.photos.update((photos) => photos.filter((x) => x.id !== photoId));
+      },
+    });
+  }
+
+  private setMainLocal(photo: Photo) {
+    const currentUser = this.accountService.currentUser();
+    if (currentUser) currentUser.imageUrl = photo.url;
+    this.accountService.setCurrentUser(currentUser as User);
+    this.memberService.member.update(
+      (member) =>
+        ({
+          ...member,
+          imageUrl: photo.url
+        }) as Member)
   }
 }
